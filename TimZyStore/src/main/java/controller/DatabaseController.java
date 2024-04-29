7c1a59a0cba6e570beb3dea31ad0c837f60b17c0
WanemIsBot <123139757+happy-kitty0821@users.noreply.github.com>
2024-04-29 22:41:17 +0545

    Update DatabaseController.java

--- TimZyStore/src/main/java/controller/DatabaseController.java
+++ TimZyStore/src/main/java/controller/DatabaseController.java
@@ -14,4 +14,29 @@ public class DatabaseController {
 		String password = "";
 		return DriverManager.getConnection(url, user, password);
 	}
+//user login method 
+	public int getUserLoginInformation(String username, String password){
+		try(connection con = getConnection()){
+			PreparedStatement statement = con.prepareStatement(utils.Get_Login_User_Information);
+			statement.setString(1, username);
+			ResultSet result = statement.executeQuery();
+			if (result.next()) {
+				String hashedPasswordFromDb = result.getString("password");
+				System.out.println("Hashed Password" + hashedPasswordFromDb);
+				if (BCrypt.checkpw(password, hashedPasswordFromDb)) {
+					String userAccountType = result.getString("accountType");
+					if ("User".equals(userAccountType)) {
+						return 1;
+					} else if ("Admin".equals(userAccountType)) {
+						return 5;
+					}
+				}
+			}
+			return 0; // Username or password is incorrect
+		}
+		catch(ClassNotFoundException | SQLException ex){
+			ex.printStackTrace();
+		}
+	}
+
 }
